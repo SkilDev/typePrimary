3adacf6555b9636f5be8d546602ecffdb8b593d4
SkilDev <eugenehorev@gmail.com>
2021-11-28 16:44:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace typePrimary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
