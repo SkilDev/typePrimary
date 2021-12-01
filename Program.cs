fc8a3956f3d6a7c68ca652153a7c7626d3eeef8c
SkilDev <eugenehorev@gmail.com>
2021-12-01 22:47:54 +0300

    Пример работы цикла

--- Program.cs
+++ Program.cs
@@ -1,16 +1,45 @@
-﻿using System;
+using System;
 
 namespace typePrimary
 {
     class Program
     {
-        /// <summary>
-        /// trend
-        /// </summary>
-        /// <param name="args"></param>
         public static void Main()
         {
-            Console.WriteLine("Hello");
+           // пример работы с циклом while
+           /* int i = 0;            
+            while (i < 5)
+            {
+                int j = 0;
+                
+                while (j < 5)
+                {                    
+                    Console.Write("[]");
+                    j++;
+                }
+                Console.WriteLine();
+                i++;
+            }*/
+
+            // Пример работы с циклом  do - while
+
+            int i = 0;
+
+            do
+            {
+                int j = 0;
+                do
+                {
+                    Console.Write("[ ]");
+                    j++;
+                    
+                } while (j < 5);
+                Console.WriteLine();
+                i++;
+                
+            } while (i < 5);
+            
         }
+
     }
 }
